95390541b42b5a3008737435318e30b0ce541161
houdek2 <houdek2@spsejecna.cz>
2024-03-18 08:13:22 +0100

    startGame() method

---
+++ src/Game.java
@@ -0,0 +1,14 @@
+public class Game {
+    public void startGame(){
+        System.out.println("""
+                 Player 1 - X
+                 Player 2 - O
+                 
+                 Input x and y coordinates of the tile you want to place your symbol in
+                 Format - > x y
+                """);
+    }
+
+
+
+}
